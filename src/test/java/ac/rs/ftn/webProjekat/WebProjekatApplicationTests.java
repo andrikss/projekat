6e34100977a76206c5ec176e9dc6cc34ce4006cb
Andrea Vasiljevic <vasiljevic.in45.2021@uns.ac.rs>
2023-08-15 01:21:32 +0200

    Dodavanje praznog projekta

---
+++ src/test/java/ac/rs/ftn/webProjekat/WebProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.rs.ftn.webProjekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
